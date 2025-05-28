a2088eb1cd3e2196f6405bbe6f19c5a9da1f78cb
ByteDance <mengdi.wang@bytedance.com>
2025-05-28 10:48:34 -0700

    update Driver with std fs

--- src/common/Driver.h
+++ src/common/Driver.h
@@ -56,15 +56,27 @@ public:
 
 	virtual void Write_Output_Files(const int frame)
 	{	
-		if(frame==0){
-			if(!File::Directory_Exists(output_dir.c_str()))
-				File::Create_Directory(output_dir);}
+		namespace fs = std::filesystem;
+		fs::path output_path(output_dir);
+		if(!fs::exists(output_path)){
+			std::cout << "#     Create output directory: " << output_dir << std::endl;
+			fs::create_directories(output_path);
+		}
 
-		frame_dir=output_dir+"/"+std::to_string(frame);
-		if(!File::Directory_Exists(frame_dir.c_str()))File::Create_Directory(frame_dir);
+		fs::path frame_path = output_path / std::to_string(frame);
+		frame_dir = frame_path.string();
+		if(!fs::exists(frame_path)){
+			std::cout << "#     Create frame directory: " << frame_path << std::endl;
+			fs::create_directories(frame_path);
+		}
+
+		//frame_dir=output_dir+"/"+std::to_string(frame);
+		//if(!File::Directory_Exists(frame_dir.c_str()))File::Create_Directory(frame_dir);
 		
-		{std::string file_name=output_dir+"/0/last_frame.txt";
-		File::Write_Text_To_File(file_name,std::to_string(frame));}
+		{
+			fs::path file_name = output_path / "0" / "last_frame.txt";
+			File::Write_Text_To_File(file_name.string(), std::to_string(frame));
+		}
 
 		if(verbose)std::cout<<"Write output files for frame "<<frame<<std::endl;
 	}
