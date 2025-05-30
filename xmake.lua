d475e69e777fbfd9e0aaa867923e9ae3aa418316
Mengdi Wang <mengdi.wang@gatech.edu>
2025-05-29 22:48:32 -0700

    cancel version restriction of libomp

--- xmake.lua
+++ xmake.lua
@@ -2,7 +2,7 @@ add_rules("mode.debug", "mode.release", "mode.releasedbg")
 set_languages("c++17")
 
 add_requires("perlinnoise >=3.0.0")
-add_requires("libomp >=19.1.0")
+add_requires("libomp")
 add_requires("tinyobjloader v2.0.0rc13")
 
 
